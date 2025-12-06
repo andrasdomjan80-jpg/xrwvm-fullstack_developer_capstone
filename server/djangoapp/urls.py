fa83d369a8f501467226e1252dcb20c9f11da8b6
“Andras <andrasdomjan80@gmail.com>
2025-12-06 04:37:12 -0500

    initial commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,19 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_user, name='logout'),
 
-    # ✅ path for get_cars
+    # path for get_cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
+
+    # path for get_dealership
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # path for get_dealer_details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
+    # path for get_dealer_reviews
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+
+    # path for add_review
+    path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
