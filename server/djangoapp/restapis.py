cf24a14aaaadfbd05d05de63469f527dcb37de6e
andrasdomjan80-jpg <andrasdomjan80@gmail.com>
2025-12-08 17:55:40 +0000

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,7 +6,11 @@ from urllib.parse import quote
 load_dotenv()
 
 # Base URLs for backend and sentiment microservice
-backend_url = os.getenv('backend_url', default="http://localhost:3030").rstrip('/')
+backend_url = os.getenv(
+    'backend_url',
+    default="http://localhost:3030"
+).rstrip('/')
+
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050"
