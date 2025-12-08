3f40ce5da6ac8a062a9626056ed4e56da6e98e94
andrasdomjan80-jpg <andrasdomjan80@gmail.com>
2025-12-08 17:51:41 +0000

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -17,7 +17,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id',
+                 models.BigAutoField(
+                     auto_created=True,
+                     primary_key=True,
+                     serialize=False,
+                     verbose_name='ID',
+                 )),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField(blank=True)),
                 ('country', models.CharField(blank=True, max_length=100)),
@@ -26,13 +32,44 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id',
+                 models.BigAutoField(
+                     auto_created=True,
+                     primary_key=True,
+                     serialize=False,
+                     verbose_name='ID',
+                 )),
                 ('dealer_id', models.IntegerField()),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'), ('COUPE', 'Coupe'), ('TRUCK', 'Truck')], default='SEDAN', max_length=10)),
-                ('year', models.IntegerField(validators=[django.core.validators.MinValueValidator(2015), django.core.validators.MaxValueValidator(2023)])),
-                ('created_at', models.DateTimeField(default=django.utils.timezone.now, editable=False)),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='models', to='djangoapp.carmake')),
+                ('type',
+                 models.CharField(
+                     choices=[
+                         ('SEDAN', 'Sedan'),
+                         ('SUV', 'SUV'),
+                         ('WAGON', 'Wagon'),
+                         ('COUPE', 'Coupe'),
+                         ('TRUCK', 'Truck'),
+                     ],
+                     default='SEDAN',
+                     max_length=10,
+                 )),
+                ('year',
+                 models.IntegerField(
+                     validators=[
+                         django.core.validators.MinValueValidator(2015),
+                         django.core.validators.MaxValueValidator(2023),
+                     ])),
+                ('created_at',
+                 models.DateTimeField(
+                     default=django.utils.timezone.now,
+                     editable=False,
+                 )),
+                ('car_make',
+                 models.ForeignKey(
+                     on_delete=django.db.models.deletion.CASCADE,
+                     related_name='models',
+                     to='djangoapp.carmake',
+                 )),
             ],
         ),
     ]
